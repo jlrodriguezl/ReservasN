2cb27125565c1495f767f79ba52b311b9e7b3043
Jose Rodríguez <jlrodriguez@asesoftware.com>
2020-09-03 22:08:50 -0500

    Operaciones Departamento Controller

--- controller/departamento.php
+++ controller/departamento.php
@@ -28,50 +28,187 @@ if(array_key_exists("idDepto", $_GET)){
 }
 
 if($_SERVER['REQUEST_METHOD']==='GET'){
-    try {
-        $query = $db->prepare('select id_depto, nom_depto from departamentos where id_depto = :idDepto');
+    if(array_key_exists("idDepto", $_GET)){
+        try {
+            $query = $db->prepare('select id_depto, nom_depto from departamentos where id_depto = :idDepto');
+            $query->bindParam(':idDepto', $idDepto);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            if($rowCount === 0){
+                $response = new Response();
+                $response->setSuccess(false);
+                $response->setHttpStatusCode(404);
+                $response->addMessage("Departamento no encontrado");
+                $response->send();
+                exit;
+            }
+
+            while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                $depto = new Departamento($row['id_depto'], $row['nom_depto']);
+                $deptoArray[] = $depto->returnDepartamentoAsArray();
+            }
+            $returnData = array();
+            $returnData['nro_filas'] = $rowCount;
+            $returnData['deptos'] = $deptoArray;
+
+            $response = new Response();
+            $response->setSuccess(true);
+            $response->setHttpStatusCode(200);
+            $response->setData($returnData);
+            $response->send();
+            exit;
+
+        } catch (DepartamentoException $ex) {
+            $response = new Response();
+            $response->setSuccess(false);
+            $response->setHttpStatusCode(500);
+            $response->addMessage($ex->getMessage());
+            $response->send();
+            exit;
+        }catch(PDOException $ex){
+            $response = new Response();
+            $response->setSuccess(false);
+            $response->setHttpStatusCode(500);
+            $response->addMessage("Error conectando a Base de Datos");
+            $response->send();
+            exit;
+        }
+    }else{
+        try{
+            $query = $db->prepare('select id_depto, nom_depto from departamentos');
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+            $deptosArray = array();
+
+            while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                $depto = new Departamento($row['id_depto'], $row['nom_depto']);
+                $deptosArray[] = $depto->returnDepartamentoAsArray();
+            }
+
+            $returnData = array();
+            $returnData['filas_retornadas'] = $rowCount;
+            $returnData['deptos'] = $deptosArray;
+
+            $response = new Response();
+            $response->setSuccess(true);
+            $response->setHttpStatusCode(200);
+            $response->toCache(true);
+            $response->setData($returnData);
+            $response->send();
+            exit;
+
+        }catch(DepartamentoException $ex){
+            $response = new Response();
+            $response->setSuccess(false);
+            $response->setHttpStatusCode(400);
+            $response->addMessage($ex->getMessage());
+            $response->send();
+            exit;
+        }catch(PDOException $ex){
+            $response = new Response();
+            $response->setSuccess(false);
+            $response->setHttpStatusCode(500);
+            $response->addMessage("Error conectando a Base de Datos");
+            $response->send();
+            exit;
+        }
+    }
+}elseif($_SERVER['REQUEST_METHOD']==='DELETE'){
+    try{
+        $query = $db->prepare('delete from departamentos where id_depto = :idDepto');
         $query->bindParam(':idDepto', $idDepto);
         $query->execute();
 
         $rowCount = $query->rowCount();
 
-        if($rowCount === 0){
+        if($rowCount===0){
             $response = new Response();
             $response->setSuccess(false);
             $response->setHttpStatusCode(404);
-            $response->addMessage("Departamento no encontrado");
+            $response->addMessage('Departamento no encontrado');
             $response->send();
-            exit;
+            exit();
         }
-
-        while($row = $query->fetch(PDO::FETCH_ASSOC)){
-            $depto = new Departamento($row['id_depto'], $row['nom_depto']);
-            $deptoArray[] = $depto->returnDepartamentoAsArray();
-        }
-        $returnData = array();
-        $returnData['nro_filas'] = $rowCount;
-        $returnData['deptos'] = $deptoArray;
-
         $response = new Response();
         $response->setSuccess(true);
         $response->setHttpStatusCode(200);
-        $response->setData($returnData);
+        $response->addMessage('Departamento eliminado');
         $response->send();
-        exit;
-
-    } catch (DepartamentoException $ex) {
-        $response = new Response();
-        $response->setSuccess(false);
-        $response->setHttpStatusCode(500);
-        $response->addMessage($ex->getMessage());
-        $response->send();
-        exit;
+        exit();
     }catch(PDOException $ex){
         $response = new Response();
         $response->setSuccess(false);
         $response->setHttpStatusCode(500);
-        $response->addMessage("Error conectando a Base de Datos");
+        $response->addMessage('Error eliminando departamento');
         $response->send();
-        exit;
+        exit();
+    }
+}elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
+    try{
+        if($_SERVER['CONTENT_TYPE'] !== 'application/json'){
+            $response = new Response();
+            $response->setSuccess(false);
+            $response->setHttpStatusCode(400);
+            $response->addMessage('Content Type no corresponde a formato JSON');
+            $response->send();
+            exit();
+        }   
+        $rawPOSTData = file_get_contents('php://input');
+        if(!$jsonData = json_decode($rawPOSTData)){
+            $response = new Response();
+            $response->setSuccess(false);
+            $response->setHttpStatusCode(400);
+            $response->addMessage('Request Body no corresponde a formato JSON');
+            $response->send();
+            exit();
+        }
+        if(!isset($jsonData->nomDepto)){
+            $response = new Response();
+            $response->setSuccess(false);
+            $response->setHttpStatusCode(400);
+            $response->addMessage('Nombre de departamento es obligatorio');
+            $response->send();
+            exit();
+        }
+        $newDepto = new Departamento(null, $jsonData->nomDepto);
+        $query = $db->prepare('insert into departamentos (nom_depto) values (:nomDepto)');
+        $query->bindParam(':nomDepto', $newDepto->getNomDepto(), PDO::PARAM_STR);
+        $query->execute();
+        $rowCount = $query->rowCount();
+        
+        if($rowCount===0){
+            $response = new Response();
+            $response->setSuccess(false);
+            $response->setHttpStatusCode(400);
+            $response->addMessage('Falló creación de departamento');
+            $response->send();
+            exit();
+        }
+        $lastIdDepto = $db->lastInsertId();
+
+        $response = new Response();
+        $response->setSuccess(true);
+        $response->setHttpStatusCode(201);
+        $response->addMessage('Departamento creado');
+        $response->setData($lastIdDepto);
+        $response->send();
+        exit();
+    }catch(DepartamentoException $ex){
+        $response = new Response();
+        $response->setSuccess(false);
+        $response->setHttpStatusCode(400);
+        $response->addMessage($ex->getMessage());
+        $response->send();
+        exit();
+    }catch(PDOException $ex){
+        $response = new Response();
+        $response->setSuccess(false);
+        $response->setHttpStatusCode(500);
+        $response->addMessage('Falló conexión a BD');
+        $response->send();
+        exit();
     }
 }
